e666be40c791d787aa9338ef5e323674a00e8fa9
Markus Andersons <markus.andersons@gmail.com>
2018-06-12 11:35:42 +0930

    Update Swagger config

--- src/main/java/com/markusandersons/hms/SwaggerConfig.java
+++ src/main/java/com/markusandersons/hms/SwaggerConfig.java
@@ -2,10 +2,11 @@ package com.markusandersons.hms;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,9 +17,18 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+            .apiInfo(getApiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+            .title("HMS API")
+            .license("Apache License Version 2.0")
+            .licenseUrl("https://github.com/markusandersons/hms/blob/master/LICENSE")
+            .build();
     }
 }
